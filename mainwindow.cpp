366f7b048a9d0a666756faac7f9f7edff4485941
marco.rasia <marcor310797@gmail.com>
2025-11-01 23:40:17 +0100

    Added all necessary frames

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,15 +7,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ////////////////////////////////////////////////////////////////
-    // Connect button state with its text and repositoryPathLabel //
-    //                 Not pressed -> show label                  //
-    //                   Pressed -> hide label                    //
-    ////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    // Connect button state with its text, repositoryPathLabel and interface //
+    ///////////////////////////////////////////////////////////////////////////
     connect(ui->RepoButton, &QPushButton::toggled, this, [=](bool checked)
     {
         ui->RepoButton->setText(checked == 0 ? "SHOW" : "UNDO");
         ui->repositoryPathLabel->setEnabled(checked == 0 ? true : false);
+        ui->InterfaceFrame->setEnabled(checked == 1 ? true : false);
     } );
 }
 
