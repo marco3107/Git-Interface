2dd4a3e1d5e2936fb0e3ca9aeaefa6dd13409fa8
marco.rasia <marcor310797@gmail.com>
2025-11-01 21:27:09 +0100

    Add button and label to insert repo's path

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ////////////////////////////////////////////////////////////////
+    // Connect button state with its text and repositoryPathLabel //
+    //                 Not pressed -> show label                  //
+    //                   Pressed -> hide label                    //
+    ////////////////////////////////////////////////////////////////
+    connect(ui->RepoButton, &QPushButton::toggled, this, [=](bool checked)
+    {
+        ui->RepoButton->setText(checked == 0 ? "SHOW" : "UNDO");
+        ui->repositoryPathLabel->setEnabled(checked == 0 ? true : false);
+    } );
 }
 
 MainWindow::~MainWindow()
